fa57489ca64f19aa5566c5492d0179daa21844ea
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-02-07 11:32:53 -0300

    Update app_emergencia.py

--- app_emergencia.py
+++ app_emergencia.py
@@ -192,7 +192,7 @@ df = get_data(archivo_usuario)
 
 st.sidebar.divider()
 st.sidebar.markdown("**Parámetros de Emergencia**")
-umbral_er = st.sidebar.slider("Umbral Tasa Diaria (Para detectar pico)", 0.05, 0.80, 0.25)
+umbral_er = st.sidebar.slider("Umbral Tasa Diaria (Para detectar pico)", 0.05, 0.80, 0.50)
 
 st.sidebar.divider()
 st.sidebar.markdown("🌡️ **Fisiología Térmica (Bio-Limit)**")
@@ -237,7 +237,7 @@ if df is not None and modelo_ann is not None:
     st.title("🌾 PREDWEEM LOLIUM- BAHIA BLANCA 2026")
 
     # --- GRÁFICO DE CALOR (HEATMAP) ---
-    colorscale_hard = [[0.0, "green"], [0.24, "green"], [0.25, "yellow"], [0.74, "yellow"], [0.75, "red"], [1.0, "red"]]
+    colorscale_hard = [[0.0, "green"], [0.49, "green"], [0.50, "yellow"], [0.79, "yellow"], [0.80, "red"], [1.0, "red"]]
     fig_risk = go.Figure(data=go.Heatmap(
         z=[df["EMERREL"].values], x=df["Fecha"], y=["Emergencia"],
         colorscale=colorscale_hard, zmin=0, zmax=1, showscale=False
